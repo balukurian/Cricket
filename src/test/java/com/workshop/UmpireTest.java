5881771c5a15e3fc52e43c19d2be74adb7837369
Balu Kurian <kurianbalu@gmail.com>
2019-08-17 23:21:21 +0530

    additional tests

--- src/test/java/com/workshop/UmpireTest.java
+++ src/test/java/com/workshop/UmpireTest.java
@@ -3,20 +3,30 @@ package com.workshop;
 import com.workshop.behaviour.BattingBehaviour;
 import com.workshop.behaviour.BowlingBehavior;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UmpireTest {
 
+    private Player normalBatsman;
+    private Player normalBowler;
+
+    @Before
+    public void init() {
+        this.normalBatsman = new Player(new BattingBehaviour(BatsmanType.NORMAL), new BowlingBehavior(BowlerType.NORMAL));
+        this.normalBowler = new Player(new BattingBehaviour(BatsmanType.NORMAL), new BowlingBehavior(BowlerType.NORMAL));
+    }
+
     @Test
     public void testWicket() {
-
-        Player normalBatsman = new Player(new BattingBehaviour(BatsmanType.NORMAL), new BowlingBehavior(BowlerType.NORMAL));
-        Player normalBowler = new Player(new BattingBehaviour(BatsmanType.NORMAL), new BowlingBehavior(BowlerType.NORMAL));
-
         BallResult ballResult = Umpire.decide(4, 4, normalBatsman, normalBowler);
-
         Assert.assertEquals(ballResult, BallResult.WICKET);
+    }
 
+    @Test
+    public void testRun() {
+        BallResult ballResult = Umpire.decide(4, 1, normalBatsman, normalBowler);
+        Assert.assertEquals(ballResult, BallResult.RUN);
     }
 
 }
